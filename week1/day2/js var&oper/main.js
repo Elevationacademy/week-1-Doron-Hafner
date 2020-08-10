4758897c2863228728e3b5f9252779e67020f84e
Doron Hafner <dh9707@gmail.com>
2020-08-10 22:42:27 +0300

    first commit added day1&2 file

---
+++ week1/day2/js var&oper/main.js
@@ -0,0 +1,43 @@
+/*-----------------exercise1-----------------
+const maxCapacity = 14
+let surfTime = true
+let beatStudent
+const purposeInLif
+purposeInLife variable will make error and beststudent will be undefined
+*/
+
+/*-----------------exercise2-----------------
+let str1 = "is the best"
+let result = str1 + " thing ever"
+console.log(result);
+*/
+
+/*-----------------exercise3-----------------
+const password = "4321"
+const confirmPassword = "1234"
+console.log(password == confirmPassword)
+*/
+
+/*-----------------exercise4-----------------
+let num1 = 423 * 12
+let num2 = 802 / 2
+let num3 = (5 + 6) / 3
+*/
+
+/*-----------------exercise5-----------------
+1 - f
+2 - t
+3 - f
+4 - t
+5 - f
+6 - t
+7 - f
+8 - f
+9 - f
+*/
+
+/*-----------------exercise6-----------------
+a = 0
+b = 0
+c = 2
+*/
