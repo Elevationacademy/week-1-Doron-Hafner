2c66155677c5683df6428f3a024afe2d0f5a59da
Doron Hafner <dh9707@gmail.com>
2020-08-28 15:10:11 +0300

    finish all js conditional statements exercises

--- day2/js conditional statement/main.js
+++ day2/js conditional statement/main.js
@@ -1,54 +1,76 @@
-/*---------------exercise1---------------
-let numChildren = 10
-let isCarful = false
-if (isCarful) {
-    console.log(numChildren);
-} else {
-    console.log(numChildren-1);
-}
-*/
-/*---------------exercise2---------------
-let silverwareCount = 100
-a= silverwareCount%2
-if (a) {
-    console.log('spmething is missing');
-} else {
-    console.log('silverwareCount is even');
-}
-*/
-/*---------------exercise3---------------
-let performance = "stellar"
-let salary = 10000
-const goodBonus = 1000
-const stellarBonus = 3000
-if (performance==="stellar") {
-    console.log(salary+stellarBonus);
-} else {
-    console.log(salary+goodBonus);
-}
-*/
-/*---------------exercise4---------------
-const isVIP = false
-let cash = 500
-if (isVIP||cash>300) {
-    console.log('Enter');
-} else {
-    console.log('stfu');
-}
-*/
-/*---------------exercise5---------------
-a=3
-b=15
-c=12
-d=160
-*/
-//---------------exercise5---------------
-const gender = null
-let profession = "business"
-if (gender==="male") {
-    console.log("businessman");
-} else if (gender==="female") {
-    console.log("businesswoman");
-} else {
-    console.log("businessperson");   
-}
+// // ---------------exercise1---------------
+// let numChildren = 10
+// let isCarful = false
+// if (isCarful) {
+//     console.log(numChildren);
+// } else {
+//     console.log(numChildren-1);
+// }
+
+// // ---------------exercise2---------------
+// let silverwareCount = 100
+// a= silverwareCount%2
+// if (a) {
+//     console.log('something is missing');
+// } else {
+//     console.log('silverwareCount is even');
+// }
+
+// // ---------------exercise3---------------
+// let performance = "stellar"
+// let salary = 10000
+// const goodBonus = 1000
+// const stellarBonus = 3000
+// if (performance==="stellar") {
+//     console.log(salary+stellarBonus);
+// } else {
+//     console.log(salary+goodBonus);
+// }
+
+// // ---------------exercise4---------------
+// const isVIP = false
+// let cash = 500
+// if (isVIP||cash>300) {
+//     console.log('Enter');
+// } else {
+//     console.log('stfu');
+// }
+
+// // ---------------exercise5---------------
+// a=3
+// b=15
+// c=12
+// d=160
+
+
+
+// //---------------exercise6---------------
+// const gender = null
+// let profession = "business"
+// if (gender==="male") {
+//     console.log("businessman");
+// } else if (gender==="female") {
+//     console.log("businesswoman");
+// } else {
+//     console.log("businessperson");   
+// }
+
+// //---------------exercise7---------------
+// let boughtTesla = true
+// const yearOfTeslaPurchase = 2014
+// let isUSCitizen = true
+// let currentYear = 2018
+// if (boughtTesla&&isUSCitizen) {
+//     if(currentYear-yearOfTeslaPurchase >= 4) {
+//         console.log('would like an upgrade?');
+//     } else {
+//         console.log('do you satisfied?');
+//     }
+// }
+// if(!isUSCitizen&&boughtTesla) {
+//     console.log('would like move to us?');
+// }
+// if (!boughtTesla) {
+//     console.log('do you interested in buying one');
+// }
+
