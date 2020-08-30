13269cd7d542b05ac19ca736c7363c1b98ce7627
Doron Hafner <dh9707@gmail.com>
2020-08-30 10:44:54 +0300

    need to finish form

---
+++ day5/form/main.js
@@ -0,0 +1,9 @@
+let name = document.getElementById('name').value
+let salary = document.getElementById('salary').value
+let bday = document.getElementById('bday').value
+let phone = document.getElementById('phone').value
+let p = document.getElementsByTagName('p')
+let checkInputs = function (){
+    if (name.length<2) {
+    }
+}
